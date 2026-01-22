7e63e0f8cfe3e13d3a1b0a7df7683500f2eb46d2
Tyyy09 <ichty086@gmail.com>
2026-01-22 16:21:53 -0500

    Add project files.

---
+++ RetroVideoGameStore/Controllers/CategoriesController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RetroVideoGameStore.Models;
+namespace RetroVideoGameStore.Controllers
+{
+    //CategoriesController class extend Controller class
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            //Use category model to create fake list of 10 categories
+            //create empty list of categories
+            var categories = new List<Category>();
+            for ( int i = 1; i<= 10; i++)
+            {
+                categories.Add(new Category
+                {
+                    Id = i,
+                    Name = "Category " + i.ToString()
+                });
+
+            }
+            return View(categories);
+        }
+        public IActionResult Browse(string categoryName)
+        {
+            // wrap the category name passed in with the URL
+            ViewBag.categoriesName = categoryName;
+
+            return View();
+        }
+        public IActionResult AddCategory()
+        {
+            //display a form for the user to add new category
+
+            return View();
+        }
+    }
+}
---
+++ RetroVideoGameStore/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace RetroVideoGameStore.Models
+{
+
+
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public required string Name { get; set; }
+        //add commen       
+    }
+}
